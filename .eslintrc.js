dad4dee7e1d3ffd8d235afa7d789c7a8d0c040bc
wontaeahn <awt0523@gmail.com>
2023-10-01 20:51:41 +0900

    (OS 별 줄 바꿈을 하는 방식이 다름으로 에러발생endOfLine: 'auto' 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,15 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    /* Delete `␍` error
+    운영체제(OS) 별 줄 바꿈을 하는 방식이 상이하기 때문에
+    endOfLine: 'auto' 추가 */
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
