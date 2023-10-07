ff74cdf2af270b160a52344d4a12c2dcd311b6a1
wontaeahn <awt0523@gmail.com>
2023-10-07 21:10:46 +0900

    ValidationPipe 적용

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // 검증 규칙이 정의되어 있지 않은 입력 값들을 모두 제거해주는 옵션
+      whitelist: true,
+      // 상세 에러 메시지를 비활성화하는 옵션입니다. 배포시는 true
+      disableErrorMessages: false,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
